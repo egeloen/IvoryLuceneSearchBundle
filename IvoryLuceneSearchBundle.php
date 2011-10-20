23a33c15550bdabf2b38b142ff0382933a8209ea
Eric GELOEN <geloen.eric@gmail.com>
2011-10-20 23:22:45 +0200

    Initial commit

---
+++ IvoryLuceneSearchBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ivory\LuceneSearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Ivory lucene search bundle
+ * 
+ * @author GeLo <geloen.eric@gmail.com>
+ */
+class IvoryLuceneSearchBundle extends Bundle
+{
+    
+}
